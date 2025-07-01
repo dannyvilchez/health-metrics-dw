60b6e326a2adb06dfd0353ef0e36a037ca735ba4
Danny Vilchez <vilchez.daniel20@gmail.com>
2025-06-30 19:00:48 -0500

    code: testing connection to db

--- migrations/migrate.py
+++ migrations/migrate.py
@@ -1,8 +1,17 @@
 import os
 import psycopg
+from psycopg import Connection
 from dotenv import load_dotenv
 
-ENV_PATH = "../.env"
+
+ENV_FILENAME = ".env"
+ENV_PATH = os.path.abspath(ENV_FILENAME)
+
+PG_HOST = print(os.getenv("PG_HOST"))
+PG_PORT = print(os.getenv("PG_PORT"))
+PG_NAME = print(os.getenv("PG_NAME"))
+PG_USER = print(os.getenv("PG_USER"))
+PG_PASSWORD = print(os.getenv("PG_PASSWORD"))
 
 
 def load_environment_variables(env: str) -> None:
@@ -11,9 +20,23 @@ def load_environment_variables(env: str) -> None:
 
     load_dotenv(env)
 
+def connect_to_database() -> Connection:
+    return psycopg.connect(
+        host = PG_HOST,
+        port = PG_PORT,
+        dbname = PG_NAME,
+        user = PG_USER,
+        password = PG_PASSWORD
+    )
 
 def main():
     load_environment_variables(ENV_PATH)
+    conn = connect_to_database()
+    
+    with conn.cursor() as cur:
+        cur.exectue("SELECT * FROM test_table;")
+        rows = cur.fetchall()
+        print(rows)
 
 
 if __name__ == "__main__":
