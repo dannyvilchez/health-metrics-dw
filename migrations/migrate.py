45f551f703d717856a1dbc0095bb184e46e478f0
Danny Vilchez <vilchez.daniel20@gmail.com>
2025-07-01 19:46:50 -0500

    validated connection to the database

--- migrations/migrate.py
+++ migrations/migrate.py
@@ -7,12 +7,6 @@ from dotenv import load_dotenv
 ENV_FILENAME = ".env"
 ENV_PATH = os.path.abspath(ENV_FILENAME)
 
-PG_HOST = print(os.getenv("PG_HOST"))
-PG_PORT = print(os.getenv("PG_PORT"))
-PG_NAME = print(os.getenv("PG_NAME"))
-PG_USER = print(os.getenv("PG_USER"))
-PG_PASSWORD = print(os.getenv("PG_PASSWORD"))
-
 
 def load_environment_variables(env: str) -> None:
     if not os.path.exists(env):
@@ -22,22 +16,23 @@ def load_environment_variables(env: str) -> None:
 
 def connect_to_database() -> Connection:
     return psycopg.connect(
-        host = PG_HOST,
-        port = PG_PORT,
-        dbname = PG_NAME,
-        user = PG_USER,
-        password = PG_PASSWORD
+        host = os.getenv("PG_HOST"),
+        port = os.getenv("PG_PORT"),
+        dbname = os.getenv("PG_NAME"),
+        user = os.getenv("PG_USER"),
+        password = os.getenv("PG_PASSWORD")
     )
 
 def main():
     load_environment_variables(ENV_PATH)
+
     conn = connect_to_database()
     
     with conn.cursor() as cur:
-        cur.exectue("SELECT * FROM test_table;")
+        cur.execute("SELECT * FROM test_table;")
         rows = cur.fetchall()
         print(rows)
 
-
 if __name__ == "__main__":
     main()
+
