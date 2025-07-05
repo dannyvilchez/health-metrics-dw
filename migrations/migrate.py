ab56b29f0573ad34989b63cba77fe6444aaff420
Danny Vilchez <vilchez.daniel20@gmail.com>
2025-07-05 08:31:58 -0500

    code: finished the migrate.py file, need to test it with an actual file

--- migrations/migrate.py
+++ migrations/migrate.py
@@ -33,7 +33,6 @@ def ensure_migrations_table_exists(conn: Connection) -> None:
             CREATE TABLE IF NOT EXISTS schema_migrations (
                 MigrationID BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 Filename TEXT UNIQUE NOT NULL,
-                FileDate TIMESTAMP NOT NULL,
                 AppliedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         """)
@@ -46,46 +45,54 @@ def get_applied_migrations(conn: Connection) -> set[str]:
         return {row[0] for row in cur.fetchall()}
 
 
-def apply_migration(conn: Connection, filename: str):
-    filedate, filename = parse_filename(filename)
+def apply_migrations(conn: Connection, file_path) -> None:
+    filename = parse_filename(os.path.basename(file_path))
+    with open(file_path, "r") as f:
+        sql = f.read()
     with conn.cursor() as cur:
+        cur.execute(sql)
         cur.execute(
-            "INSERT INTO schema_migrations (Filename, FileDate) VALUES (%s, %s);",
-            (filename, filedate),
+            "INSERT INTO schema_migrations (Filename) VALUES (%s);",
+            (filename),
         )
     cur.commit()
 
 
-def parse_filename(filename: str) -> tuple[datetime, str]:
-    match = re.match(r"^(\d{8})_(.+)\.sql$", filename)
+def parse_filename(filename: str) -> str:
+    match = re.match(r"^(\d{8}_.+)\.sql$", filename)
 
     if not match:
         raise ValueError(
             f"Filename '{filename}' is not in the expected format: YYYYMMDD_filename.sql"
         )
 
-    filedate = datetime.strptime(match.group(1), "%Y%m%d")
-    filename = match.group(2)
+    filename = match.group(1)
 
-    return filedate, filename
+    return filename
 
 
 def main():
     load_environment_variables(ENV_PATH)
-
     conn = connect_to_database()
 
     ensure_migrations_table_exists(conn)
+    applied = get_applied_migrations(conn)
+    file_list = sorted(os.listdir("migrations"))
+
+    """
+    This the main loop that applies the migrations to the database.
+    I thought about abstracting this out to its own function but 
+    decided against it because there's no major complexity here that 
+    I want to develop or test separately. And there's no branching in it.
+    """
+    for file in all_files:
+        if file.endswith(".sql") and file not in applied:
+            apply_migrations(conn, os.path.join("migrations", file))
 
     with conn.cursor() as cur:
-        cur.execute("""
-            INSERT INTO schema_migrations (filename) VALUES ('test_file');
-        """)
         cur.execute("SELECT * FROM schema_migrations")
         rows = cur.fetchall()
         print(rows)
-
-    applied = get_applied_migrations(conn)
 
     print(applied)
 
